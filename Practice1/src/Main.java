05bd1f39cddc9d371fe975564e082daf1f89b2f7
Windawz <46968683+Windawz@users.noreply.github.com>
2023-02-17 22:16:53 +0300

    Refactor main

--- Practice1/src/Main.java
+++ Practice1/src/Main.java
@@ -21,6 +21,7 @@ public class Main {
 
     public static void main(String[] args) {
         var stats = makeRandomStats(7);
+
         for (var stat : stats) {
             if (stat != null) {
                 stat.show();
@@ -40,57 +41,73 @@ public class Main {
         }
         System.out.println();
 
-        var sum = Arrays.stream(stats)
-            .filter(Objects::nonNull)
-            .map(stat -> stat.getTotal())
-            .reduce((acc, item) -> acc + item)
-            .get();
-        System.out.println("Total expenses sum = " + sum);
+        var totalExpensesSum = getTotalExpensesSum(stats);
+        System.out.println("Total expenses sum = " + totalExpensesSum);
         System.out.println();
 
-        var maxTotalExpensesAccountName = Arrays.stream(stats)
+        var maxTotalExpensesAccountName = getMaxTotalExpensesAccountName(stats);
+        System.out.println("Account with greatest total expenses: " + maxTotalExpensesAccountName);
+        System.out.println();
+    }
+
+    private static String getMaxTotalExpensesAccountName(ExpenseStat[] stats) {
+        return Arrays.stream(stats)
             .filter(Objects::nonNull)
             .max(Comparator.comparingInt(ExpenseStat::getTotal))
             .get()
             .getAccountName();
-        System.out.println("Account with greatest total expenses: " + maxTotalExpensesAccountName);
-        System.out.println();
+    }
+
+    private static int getTotalExpensesSum(ExpenseStat[] stats) {
+        return Arrays.stream(stats)
+            .filter(Objects::nonNull)
+            .map(ExpenseStat::getTotal)
+            .reduce(Integer::sum)
+            .get();
     }
 
     private static ExpenseStat[] makeRandomStats(int count) {
         if (count < 0) {
             throw new IllegalArgumentException("Count may not be negative");
         }
-        var namePool = new HashSet<String>(ACCOUNT_NAMES.length);
+
+        var usedNames = new HashSet<String>(ACCOUNT_NAMES.length);
         var random = new Random(makeSeedFromCurrentTime());
         var stats = new ExpenseStat[count];
+
         for (int i = 0; i < stats.length; i++) {
             if (i == 2) {
                 stats[i] = null;
             } else if (i == stats.length - 1) {
                 stats[i] = new ExpenseStat();
             } else {
-                stats[i] = makeRandomStat(random, namePool);
+                stats[i] = makeRandomStat(random, usedNames);
             }
         }
+
         return stats;
     }
 
-    private static ExpenseStat makeRandomStat(Random random, Set<String> namePool) {
-        String name = null;
-        if (namePool.size() < ACCOUNT_NAMES.length) {
-            do {
-                name = ACCOUNT_NAMES[random.nextInt(0, ACCOUNT_NAMES.length)];
-            } while (namePool.contains(name));
-            namePool.add(name);
-        }
-
+    private static ExpenseStat makeRandomStat(Random random, Set<String> usedNames) {
         return new ExpenseStat(
             random.nextInt(10, 200),
-            name,
+            pickRandomName(random, usedNames),
             random.nextInt(400, 1000),
             random.nextInt(2, 20)
         );
+    }
+
+    private static String pickRandomName(Random random, Set<String> usedNames) {
+        if (usedNames.size() < ACCOUNT_NAMES.length) {
+            String name = null;
+            do {
+                name = ACCOUNT_NAMES[random.nextInt(0, ACCOUNT_NAMES.length)];
+            } while (usedNames.contains(name));
+            usedNames.add(name);
+            return name;
+        } else {
+            throw new IllegalStateException("All names have been used up");
+        }
     }
 
     private static long makeSeedFromCurrentTime() {
