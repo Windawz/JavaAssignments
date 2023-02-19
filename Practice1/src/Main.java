9a7abd35500040335e8f41b55fe54b7e5cd68aad
Windawz <46968683+Windawz@users.noreply.github.com>
2023-02-19 12:24:17 +0300

    Replace arrays with lists

--- Practice1/src/Main.java
+++ Practice1/src/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.util.function.Predicate;
 
 public class Main {
-    private static final String[] ACCOUNT_NAMES = {
+    private static final List<String> ACCOUNT_NAMES = List.of(
         "Kevin Parcel",
         "David Pratt",
         "Bertram Reynolds",
@@ -16,11 +16,10 @@ public class Main {
         "Amy Williams",
         "Andrew Wood",
         "Simon Wood",
-        "Jeremy Wright",
-    };
+        "Jeremy Wright");
 
     public static void main(String[] args) {
-        var stats = makeRandomStats(7);
+        var stats = makeRandomStats(ACCOUNT_NAMES, 7);
 
         for (var stat : stats) {
             if (stat != null) {
@@ -30,10 +29,10 @@ public class Main {
         }
 
         System.out.println("Setting transportation expenses on the last item...");
-        stats[stats.length - 1].setTransportationExpenses(666);
+        stats.get(stats.size() - 1).setTransportationExpenses(666);
         System.out.println();
 
-        System.out.println("Duration = " + (stats[0].getDayCount() + stats[1].getDayCount()));
+        System.out.println("Duration = " + (stats.get(0).getDayCount() + stats.get(1).getDayCount()));
         System.out.println();
 
         for (var stat : stats) {
@@ -52,57 +51,56 @@ public class Main {
         System.out.println();
     }
 
-    private static String getMaxTotalExpensesAccountName(ExpenseStat[] stats) {
-        return Arrays.stream(stats)
+    private static String getMaxTotalExpensesAccountName(List<ExpenseStat> stats) {
+        return stats.stream()
             .filter(stat -> stat != null)
             .max(Comparator.comparingInt(stat -> stat.getTotal()))
             .get()
             .getAccountName();
     }
 
-    private static int getTotalExpensesSum(ExpenseStat[] stats) {
-        return Arrays.stream(stats)
+    private static int getTotalExpensesSum(List<ExpenseStat> stats) {
+        return stats.stream()
             .filter(stat -> stat != null)
             .map(stat -> stat.getTotal())
             .reduce((acc, value) -> acc + value)
             .get();
     }
 
-    private static ExpenseStat[] makeRandomStats(int count) {
+    private static List<ExpenseStat> makeRandomStats(List<String> accountNames, int count) {
         if (count < 0) {
             throw new IllegalArgumentException("Count may not be negative");
         }
 
         var random = makeRandom();
 
-        var rates = getRandomInts(random, 100, 2000);
-        var accountNames = getRandomNames(random, ACCOUNT_NAMES);
-        var expenses = getRandomInts(random, 0, 2000);
-        var dayCounts = getRandomInts(random, 2, 31);
+        var rates = getRandomInts(random, 100, 2000, count);
+        var shuffledAccountNames = getShuffledList(random, accountNames, count);
+        var expenses = getRandomInts(random, 0, 2000, count);
+        var dayCounts = getRandomInts(random, 2, 31, count);
 
-        var stats = new ExpenseStat[count];
-        for (int i = 0; i < stats.length; i++) {
+        var stats = new ArrayList<ExpenseStat>(count);
+        for (int i = 0; i < count; i++) {
             if (i == 2) {
-                stats[i] = null;
-            } else if (i == stats.length - 1) {
-                stats[i] = new ExpenseStat();
+                stats.add(null);
+            } else if (i == count - 1) {
+                stats.add(new ExpenseStat());
             } else {
-                stats[i] = new ExpenseStat(
+                stats.add(new ExpenseStat(
                     rates.get(i),
-                    accountNames.get(i),
+                    shuffledAccountNames.get(i),
                     expenses.get(i),
-                    dayCounts.get(i));
+                    dayCounts.get(i)));
             }
         }
 
         return stats;
     }
 
-    private static List<Integer> getRandomInts(Random random, int origin, int bound) {
+    private static List<Integer> getRandomInts(Random random, int origin, int bound, int count) {
         if (origin >= bound) {
             throw new IllegalArgumentException("Origin may not be greater than or equal to bound");
         }
-        var count = bound - origin;
         var list = new ArrayList<Integer>(count);
         for (int i = 0; i < count; i++) {
             list.add(random.nextInt(origin, bound));
@@ -110,12 +108,13 @@ public class Main {
         return list;
     }
 
-    private static List<String> getRandomNames(Random random, String[] names) {
-        return getShuffledList(random, Arrays.asList(names));
-    }
-
-    private static <T> List<T> getShuffledList(Random random, List<T> list) {
-        int count = list.size();
+    private static <T> List<T> getShuffledList(Random random, List<T> list, int count) {
+        if (count > list.size()) {
+            throw new IllegalArgumentException("Count may not exceed list size");
+        }
+        if (count < 0) {
+            throw new IllegalArgumentException("Count may not be negative");
+        }
         var shuffledIndices = new ArrayList<Integer>(count);
         for (int i = 0; i < count; i++) {
             int shuffledIndex = getRandomIntUntil(random, 0, count, x -> !shuffledIndices.contains(x));
