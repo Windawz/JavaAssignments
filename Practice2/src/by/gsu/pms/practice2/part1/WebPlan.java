8dc23ee4b11566d29a07eb54df350db8862a8657
Windawz <46968683+Windawz@users.noreply.github.com>
2023-03-02 10:22:02 +0300

    Add WebPlan class

---
+++ Practice2/src/by/gsu/pms/practice2/part1/WebPlan.java
@@ -0,0 +1,18 @@
+package by.gsu.pms.practice2.part1;
+
+import by.gsu.pms.practice2.Money;
+import by.gsu.pms.practice2.Period;
+import by.gsu.pms.practice2.Traffic;
+
+public class WebPlan extends Plan {
+    public WebPlan(Money fee, Period period, Traffic traffic) {
+        super(fee, period);
+        _traffic = traffic;
+    }
+
+    private final Traffic _traffic;
+
+    public Traffic getTraffic() {
+        return _traffic;
+    }
+}
