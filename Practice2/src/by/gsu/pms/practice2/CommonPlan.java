095e9c426c45f301ff065f7196b0a90f58b2d730
Windawz <46968683+Windawz@users.noreply.github.com>
2023-03-03 15:29:29 +0300

    Replace concats with appends

--- Practice2/src/by/gsu/pms/practice2/CommonPlan.java
+++ Practice2/src/by/gsu/pms/practice2/CommonPlan.java
@@ -13,22 +13,34 @@ public interface CommonPlan {
     default String getInfoString() {
         var builder = new StringBuilder();
         tryGetFee().ifPresent(
-            fee -> builder.append("Fee: " + fee + "; "));
+            fee -> builder
+                .append("Fee: ")
+                .append(fee)
+                .append("; "));
         tryGetPeriod().ifPresent(
-            period -> builder.append("Period: " + period + "; "));
+            period -> builder
+                .append("Period: ")
+                .append(period)
+                .append("; "));
         tryGetTraffic().ifPresent(
-            traffic -> builder.append("Traffic: " + traffic + "; "));
+            traffic -> builder
+                .append("Traffic: ")
+                .append(traffic)
+                .append("; "));
         tryGetCallTime().ifPresent(
-            callTime -> builder.append("Call Time: " + callTime + "; "));
+            callTime -> builder
+                .append("Call Time: ")
+                .append(callTime)
+                .append("; "));
         tryGetForeignCountries().ifPresent(
-            foreignCountries -> builder.append(
-                "Foreign Countries: "
+            foreignCountries -> builder
+                .append("Foreign Countries: "
                     + String.join(
-                    ", ",
-                    foreignCountries
-                        .stream()
-                        .map(country -> country.toString())
-                        .toList())
+                        ", ",
+                        foreignCountries
+                            .stream()
+                            .map(country -> country.toString())
+                            .toList())
                     + ";"));
         return builder.toString();
     }
