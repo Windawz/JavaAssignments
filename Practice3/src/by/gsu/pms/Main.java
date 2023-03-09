99a3b590576e2005f4523da77948f4d45396f5eb
Windawz <46968683+Windawz@users.noreply.github.com>
2023-03-09 10:55:40 +0300

    Implement main program logic

--- Practice3/src/by/gsu/pms/Main.java
+++ Practice3/src/by/gsu/pms/Main.java
@@ -1,7 +1,11 @@
 package by.gsu.pms;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try (var session = new Session("managers.ser")) {
+            session.run();
+        }
     }
 }
--- Practice3/src/by/gsu/pms/Manager.java
+++ Practice3/src/by/gsu/pms/Manager.java
@@ -1,9 +1,8 @@
 package by.gsu.pms;
 
-import java.util.Optional;
-import java.util.Scanner;
+import java.io.Serializable;
 
-public class Manager {
+public class Manager implements Serializable {
     public Manager(String surname, int age, boolean isCompetent) {
         _surname = surname;
         _age = age;
@@ -13,41 +12,6 @@ public class Manager {
     private final String _surname;
     private final int _age;
     private final boolean _isCompetent;
-
-    public static Manager scan(Scanner scanner) {
-        var lambdaContext = new Object() {
-            String surname;
-            int age;
-            boolean isCompetent;
-        };
-
-        var parsers = new Parser<?>[] {
-            new SurnameParser(
-                value -> lambdaContext.surname = value),
-            new AgeParser(
-                value -> lambdaContext.age = value),
-            new CompetenceParser(
-                value -> lambdaContext.isCompetent = value),
-        };
-
-        for (var parser : parsers) {
-            String message = parser.getMessage();
-            Optional<ParseError> maybeError = Optional.empty();
-            do {
-                System.out.println(parser.getMessage());
-                String input = scanner.nextLine();
-                maybeError = parser.parseAndAssign(input);
-                if (maybeError.isPresent()) {
-                    System.out.println("Invalid value. Try again.");
-                }
-            } while (maybeError.isPresent());
-        }
-
-        return new Manager(
-            lambdaContext.surname,
-            lambdaContext.age,
-            lambdaContext.isCompetent);
-    }
 
     public String getSurname() {
         return _surname;
