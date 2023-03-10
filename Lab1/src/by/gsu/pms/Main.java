f65267d0f7be1489251a843e64a0ca88ca28838f
Windawz <46968683+Windawz@users.noreply.github.com>
2023-03-10 21:18:04 +0300

    Implement main logic as per task

--- Lab1/src/by/gsu/pms/Main.java
+++ Lab1/src/by/gsu/pms/Main.java
@@ -1,8 +1,16 @@
 package by.gsu.pms;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        try (var session = new Session()) {
+        var managers = List.of(
+            new Manager("Petrovsky", 23, true),
+            new Manager("Grachev", 31, true),
+            new Manager("Mazurov", 22, false),
+            new Manager("Babkin", 29, false),
+            new Manager("Trubetskoy", 30, true));
+        try (var session = new Session(managers)) {
             session.run();
         }
     }
--- Lab1/src/by/gsu/pms/Session.java
+++ Lab1/src/by/gsu/pms/Session.java
@@ -1,46 +1,96 @@
 package by.gsu.pms;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
-import java.util.function.Function;
+import java.util.Objects;
 
 public class Session implements AutoCloseable {
-    public Session() {
+    /**
+     * @param managers
+     *     may contain nulls.
+     */
+    public Session(List<Manager> managers) {
+        _managers = managers.stream()
+            .filter(Objects::nonNull)
+            .toList();
     }
-
-    private final List<Manager> _managers = List.of();
+    
+    private final List<Manager> _managers;
     private boolean _closed = false;
-
+    
     public void run() {
-
+        throwIfClosed();
+        if (_managers.isEmpty()) {
+            System.out.println("No managers provided.");
+        }
+        printCompetentManagerCount();
+        printAverageAgeOfManagers();
+        printManagers();
+        printSortedManagers();
+        printManagers();
     }
-
+    
     @Override
     public void close() throws Exception {
         if (!_closed) {
             _closed = true;
         }
     }
-
+    
     private void printManagers() {
-        for (var manager : _managers) {
-            System.out.println(
-                MessageFormat.format(
-                    "Manager {0} is {1} years old and is {2}",
-                    manager.getSurname(),
-                    manager.getAge(),
-                    manager.isCompetent() ? "competent" : "incompetent"));
-        }
+        System.out.println("Managers:");
+        _managers.forEach(Session::printManager);
     }
-
+    
+    private void printSortedManagers() {
+        System.out.println("Sorted managers:");
+        _managers.stream()
+            .sorted(Session::compareManagers)
+            .forEach(Session::printManager);
+    }
+    
+    private void printAverageAgeOfManagers() {
+        System.out.println("Average age:");
+        System.out.println(
+            _managers.stream()
+                .map(Manager::getAge)
+                .reduce(0, Integer::sum)
+                / _managers.size());
+    }
+    
+    private void printCompetentManagerCount() {
+        System.out.println("Competent manager count:");
+        System.out.println(
+            _managers.stream()
+                .filter(Manager::isCompetent)
+                .count());
+    }
+    
     private void throwIfClosed() {
         if (_closed) {
             throw new IllegalStateException("Session has been closed");
         }
+    }
+    
+    private static void printManager(Manager manager) {
+        System.out.println(
+            MessageFormat.format(
+                "Manager {0} is {1} years old and is {2}",
+                manager.getSurname(),
+                manager.getAge(),
+                manager.isCompetent() ? "competent" : "incompetent"));
+    }
+    
+    private static int compareManagers(Manager left, Manager right) {
+        int difference = left.getSurname().compareTo(right.getSurname());
+        if (difference == 0) {
+            difference = Integer.compare(left.getAge(), right.getAge());
+        }
+        if (difference == 0) {
+            difference = Boolean.compare(
+                left.isCompetent(),
+                right.isCompetent());
+        }
+        return difference;
     }
 }
