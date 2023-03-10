a7e81a9c93c16b38255687e0afad23ecaadeacfb
Windawz <46968683+Windawz@users.noreply.github.com>
2023-03-10 20:58:26 +0300

    Add overloaded ctor to Manager

--- Lab1/src/by/gsu/pms/Manager.java
+++ Lab1/src/by/gsu/pms/Manager.java
@@ -3,24 +3,28 @@ package by.gsu.pms;
 import java.io.Serializable;
 
 public class Manager implements Serializable {
+    public Manager() {
+        this("Unnamed", -1, false);
+    }
+    
     public Manager(String surname, int age, boolean isCompetent) {
         _surname = surname;
         _age = age;
         _isCompetent = isCompetent;
     }
-
+    
     private final String _surname;
     private final int _age;
     private final boolean _isCompetent;
-
+    
     public String getSurname() {
         return _surname;
     }
-
+    
     public int getAge() {
         return _age;
     }
-
+    
     public boolean isCompetent() {
         return _isCompetent;
     }
